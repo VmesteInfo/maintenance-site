7093f4f1d1a59295eb2839f13e9cd3f06f828fea
Art Ginzburg <ginzburg.arthur@gmail.com>
2026-01-29 22:08:33 +0100

    Configure custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://VmesteAdmin.github.io',
-  base: '/maintenance-site',
+  site: 'https://vmeste.info',
   integrations: [tailwind()],
 });
